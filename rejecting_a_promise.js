d0cfa05ac6fb3cf89979730964ece5a0f8d76997
TBtuo <tuo.zhang@telecom-bretagne.eu>
2016-08-12 16:11:15 +0200

    reject a promise

---
+++ rejecting_a_promise.js
@@ -0,0 +1,16 @@
+/**
+ * Created by TBtuo on 12/08/16.
+ */
+
+var promise = new Promise(function (fulfill, reject) {
+
+    setTimeout(function () {
+        reject(new Error('REJECTED'));
+    },300);
+});
+
+function onReject(error) {
+    console.log(error.message)
+}
+
+promise.then(null,onReject);
