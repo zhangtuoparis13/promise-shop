d22f91b330b3c8fe786fbb309515a43aa827c817
TBtuo <tuo.zhang@telecom-bretagne.eu>
2016-08-12 15:45:29 +0200

    fulfill a promise

---
+++ fullfillPromise.js
@@ -0,0 +1,32 @@
+
+/*var promise = new Promise(function (fullfill, reject) {
+    setTimeout(function () {
+        fullfill('FULLFILLED!')
+    },300);
+});
+
+promise.then(function (value) {
+    console.log(value);
+}).catch(function (error) {
+    console.log(error);
+});*/
+
+'use strict';
+
+// Create a promise
+
+var promise = new Promise(function (fulfill, reject) {
+  // After the timeout reaches 300ms, fulfill the promise with value
+  // 'FULFILLED!'.
+
+  setTimeout(function () {
+    fulfill('FULFILLED!');
+  }, 300);
+});
+
+// Add a handler to the promise’s fulfillment. `console.log` will be called
+// with the value passed to `fulfill`, which is `'FULFILLED!'`.
+// Note that this statement will ALWAYS be executed before `fulfill` is
+// called (we'll talk about this in depth in the lessons to come).
+
+promise.then(console.log);
