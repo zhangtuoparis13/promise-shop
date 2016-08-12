f184d561fe043dce282cdbbe617b5930126e30b7
TBtuo <tuo.zhang@telecom-bretagne.eu>
2016-08-12 11:27:25 +0200

    ok the first two

--- setTimeOut.js
+++ setTimeOut.js
@@ -1,3 +1,13 @@
 /**
  * Created by TBtuo on 12/08/16.
  */
+
+// function myfunc() {
+//     console.log('TIMED OUT!')
+// }
+// let mytimeout = setTimeout(myfunc,300);
+// clearTimeout(mytimeout);
+
+setTimeout(function () {
+    console.log('TIMED OUT!');
+},300);
